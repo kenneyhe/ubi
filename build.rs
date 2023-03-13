a2392813ac4c894a7909fa4d058ec5828fbb881e
K <kenneyhe@users.noreply.github.com>
2023-03-13 15:07:48 -0700

    Update build.rs
    Add openssl_dir

--- build.rs
+++ build.rs
@@ -7,6 +7,7 @@ fn main() {
     // https://doc.rust-lang.org/cargo/reference/environment-variables.html#environment-variables-cargo-sets-for-build-scripts
     // So we just re-export them to the crate code.
     println!("cargo:rustc-env=TARGET={}", env::var("TARGET").unwrap());
+    println!("cargo:rustc-env=OPENSSL_DIR=/usr/include/openssl/", env::var("OPENSSL_DIR").unwrap());
     // By default Cargo only runs the build script when a file changes.  This
     // makes it re-run on target change
     println!("cargo:rerun-if-changed-env=TARGET");
